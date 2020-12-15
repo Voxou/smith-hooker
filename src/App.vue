cfef71380705f595ce7760a1ebabe21f3b5b5f03
Manon Julien-Kuentz <julienkuentzmanon@gmail.com>
2020-12-15 10:27:43 +0100

    revert test

--- src/App.vue
+++ src/App.vue
@@ -9,7 +9,7 @@ import { IonApp, IonRouterOutlet } from '@ionic/vue';
 import { defineComponent } from 'vue';
 
 export default defineComponent({
-  name: 'POC App',
+  name: 'App',
   components: {
     IonApp,
     IonRouterOutlet
