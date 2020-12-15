9b7eaa016f8a72ca30136d61fd7ec6a8a183b953
Manon Julien-Kuentz <julienkuentzmanon@gmail.com>
2020-12-15 17:38:44 +0100

    LW-35 Page d'accueil + navbar

--- src/views/Tabs.vue
+++ src/views/Tabs.vue
@@ -3,36 +3,54 @@
     <ion-tabs>
       <ion-tab-bar slot="bottom">
         <ion-tab-button tab="tab1" href="/tabs/tab1">
-          <ion-icon :icon="triangle" />
-          <ion-label>Tab 1</ion-label>
+          <ion-icon :icon="mapOutline" />
         </ion-tab-button>
           
         <ion-tab-button tab="tab2" href="/tabs/tab2">
-          <ion-icon :icon="ellipse" />
-          <ion-label>Tab 2</ion-label>
+          <ion-icon :icon="peopleCircleOutline" />
         </ion-tab-button>
         
-        <ion-tab-button tab="tab3" href="/tabs/tab3">
-          <ion-icon :icon="square" />
-          <ion-label>Tab 3</ion-label>
+        <ion-tab-button tab="tab3"></ion-tab-button>
+
+        <!-- <ion-tab-button tab="tab3" href="/tabs/tab3">
+          <ion-icon :icon="homeOutline" />
+          <ion-label>Accueil</ion-label>
+        </ion-tab-button> -->
+
+        <ion-tab-button tab="tab4" href="/tabs/tab4">
+          <ion-icon :icon="personOutline" />
         </ion-tab-button>
+
+        <ion-tab-button tab="tab5" href="/tabs/tab5">
+          <ion-icon :icon="settingsOutline" />
+        </ion-tab-button>
+
       </ion-tab-bar>
     </ion-tabs>
+
+        <ion-fab vertical="bottom" horizontal="center" slot="fixed">
+          <ion-fab-button href="/tabs/tab3">
+              <ion-icon :icon="homeOutline" />
+          </ion-fab-button>
+        </ion-fab>
+    
   </ion-page>
 </template>
 
 <script lang="ts">
-import { IonTabBar, IonTabButton, IonTabs, IonLabel, IonIcon, IonPage } from '@ionic/vue';
-import { ellipse, square, triangle } from 'ionicons/icons';
+import { IonTabBar, IonTabButton, IonTabs, IonIcon, IonPage } from '@ionic/vue';
+import { homeOutline, personOutline, mapOutline, settingsOutline, peopleCircleOutline } from 'ionicons/icons';
 
 export default {
   name: 'Tabs',
-  components: { IonLabel, IonTabs, IonTabBar, IonTabButton, IonIcon, IonPage },
+  components: { IonTabs, IonTabBar, IonTabButton, IonIcon, IonPage },
   setup() {
     return {
-      ellipse, 
-      square, 
-      triangle,
+      homeOutline,
+      personOutline,
+      mapOutline,
+      settingsOutline,
+      peopleCircleOutline
     }
   }
 }
