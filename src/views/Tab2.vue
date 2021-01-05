f5fc7bb05e2bb4b924608cf8fb058f74c8c81af9
DylanCampA <dylan.brito@campus.academy>
2021-01-05 11:47:16 +0100

    Front Nos Artisants + scroll to top

--- src/views/Tab2.vue
+++ src/views/Tab2.vue
@@ -2,27 +2,66 @@
   <ion-page>
     <ion-header>
       <ion-toolbar>
-        <ion-title>Artisans</ion-title>
+        <ion-title>Nos Artisans</ion-title>
       </ion-toolbar>
     </ion-header>
-    <ion-content :fullscreen="true">
-      <ion-header collapse="condense">
-        <ion-toolbar>
-          <ion-title size="large">Artisans</ion-title>
-        </ion-toolbar>
-      </ion-header>
-    
-      <ExploreContainer name="Artisans page" />
+    <ion-content :scroll-events="true" ref="content">
+      
+      <ion-card>
+        <img src="https://images.unsplash.com/photo-1517433670267-08bbd4be890f?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=972&q=80" alt="" />
+        <ion-card-header>
+          <ion-card-title>Le bon pain - Boulangerie n°1</ion-card-title>
+        </ion-card-header>
+        <ion-card-content class="Text">
+          Keep close to Nature's heart... and break clear away, once in awhile,
+          and climb a mountain or spend a week in the woods. Wash your spirit clean.
+        </ion-card-content>
+        <ion-button size="small" style="float: right;">Default</ion-button>
+      </ion-card>
+
+      <ion-card>
+        <img src="https://www.tabacpresseleliberte.fr/images/tabac-presse-leliberte-3-castelnau-le-lez-34170.JPG" alt="" />
+        <ion-card-header>
+          <ion-card-title>Tabac Press Loto</ion-card-title>
+        </ion-card-header>
+        <ion-card-content class="Text">
+          Keep close to Nature's heart... and break clear away, once in awhile,
+          and climb a mountain or spend a week in the woods. Wash your spirit clean.
+        </ion-card-content>
+        <ion-button size="small" style="float: right;">Default</ion-button>
+      </ion-card>    
+
+      <ion-fab vertical="bottom" horizontal="end" slot="fixed">
+      <ion-fab-button color="light" @click="scroll">
+        <ion-icon :icon="chevronUpOutline"></ion-icon>
+      </ion-fab-button>
+    </ion-fab>
+
+    <ion-footer class="Footer"></ion-footer>
+
     </ion-content>
   </ion-page>
 </template>
 
 <script lang="ts">
-import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
-import ExploreContainer from '@/components/ExploreContainer.vue';
+import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonCardTitle, IonCardHeader, IonCardContent, IonCard, IonFab, IonFabButton, IonIcon } from '@ionic/vue';
+import { chevronUpOutline } from 'ionicons/icons';
+import { ref } from 'vue';
 
 export default  {
   name: 'Tab2',
-  components: { ExploreContainer, IonHeader, IonToolbar, IonTitle, IonContent, IonPage }
+  components: { IonHeader, IonToolbar, IonTitle, IonContent, IonPage, IonCardTitle, IonCardHeader, IonCardContent, IonCard, IonFab, IonFabButton, IonIcon },
+  setup() {
+    const content = ref();
+    const scroll = async () => {
+      await  content.value.$el.scrollToTop()
+      //debugger;
+    };
+    return{
+      chevronUpOutline, 
+      scroll,
+      content
+    }
+  }
 }
 </script>
--- src/views/Tab3.vue
+++ src/views/Tab3.vue
@@ -5,14 +5,7 @@
         <ion-title>Accueil</ion-title>
       </ion-toolbar>
     </ion-header>
-    <ion-content 
-    :scroll-events="true"
-    >
-      <ion-header collapse="condense">
-        <ion-toolbar>
-          <ion-title size="large">Tab 2</ion-title>
-        </ion-toolbar>
-      </ion-header>
+    <ion-content :scroll-events="true" ref="content">
       
       <ion-card>
         <img src="https://images.unsplash.com/photo-1565349173908-1cf6bc9fd4ee?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80" alt=""/>
@@ -20,7 +13,7 @@
           <ion-card-subtitle>Lieux incontournables</ion-card-subtitle>
           <ion-card-title>Découvrir la ville</ion-card-title>
         </ion-card-header>
-        <ion-card-content>
+        <ion-card-content class="Text">
           Keep close to Nature's heart... and break clear away, once in awhile,
           and climb a mountain or spend a week in the woods. Wash your spirit clean.
         </ion-card-content>
@@ -32,7 +25,7 @@
           <ion-card-subtitle>Nos artisans</ion-card-subtitle>
           <ion-card-title>Le bon pain - Boulangerie n°1</ion-card-title>
         </ion-card-header>
-        <ion-card-content>
+        <ion-card-content class="Text">
           Keep close to Nature's heart... and break clear away, once in awhile,
           and climb a mountain or spend a week in the woods. Wash your spirit clean.
         </ion-card-content>
@@ -43,13 +36,15 @@
           <ion-card-subtitle>Card Subtitle</ion-card-subtitle>
           <ion-card-title>Card Title</ion-card-title>
         </ion-card-header>
-        <ion-card-content>
+        <ion-card-content class="Text">
           Keep close to Nature's heart... and break clear away, once in awhile,
           and climb a mountain or spend a week in the woods. Wash your spirit clean.
         </ion-card-content>
       </ion-card>
+
+       <ion-footer class="Footer"></ion-footer>
      
-     <ion-fab vertical="bottom" horizontal="end" slot="fixed">
+     <ion-fab vertical="bottom" horizontal="end" slot="fixed" @click="scroll">
       <ion-fab-button color="light">
         <ion-icon :icon="chevronUpOutline"></ion-icon>
       </ion-fab-button>
@@ -61,15 +56,23 @@
 </template>
 
 <script lang="ts">
-import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonCardSubtitle, IonCardTitle, IonCardHeader, IonCardContent, IonCard, IonFab, IonFabButton, IonIcon } from '@ionic/vue';
+import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonCardTitle, IonCardHeader, IonCardContent, IonCard, IonFab, IonFabButton, IonIcon } from '@ionic/vue';
 import { chevronUpOutline } from 'ionicons/icons';
+import { ref } from 'vue';
 
 export default  {
   name: 'Tab2',
-  components: { IonHeader, IonToolbar, IonTitle, IonContent, IonPage, IonCardSubtitle, IonCardTitle, IonCardHeader, IonCardContent, IonCard, IonFab, IonFabButton, IonIcon },
+  components: { IonHeader, IonToolbar, IonTitle, IonContent, IonPage, IonCardTitle, IonCardHeader, IonCardContent, IonCard, IonFab, IonFabButton, IonIcon },
   setup() {
+    const content = ref();
+    const scroll = async () => {
+      await  content.value.$el.scrollToTop()
+      //debugger;
+    };
     return{
-      chevronUpOutline
+      chevronUpOutline, 
+      scroll,
+      content
     }
   }
 }
