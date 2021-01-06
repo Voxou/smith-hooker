6aa6421f99909e337a804d152b329421addf2864
QuentinNEP <pro.quentin@hotmail.com>
2021-01-06 11:14:39 +0100

    add map

--- src/views/Tab1.vue
+++ src/views/Tab1.vue
@@ -10,12 +10,42 @@
         <ion-toolbar>
           <ion-title size="large">Carte</ion-title>
         </ion-toolbar>
+        <div id="map"></div>
       </ion-header>
     
       <ExploreContainer name="Carte page" />
     </ion-content>
   </ion-page>
 </template>
+
+<script lang="js">
+  let map;
+
+  function initMap() {
+    map = new google.maps.Map(document.getElementById("map"), {
+      center: { lat:48.8534, lng:2.3488},
+      zoom: 8,
+    });
+    
+    addMarker(48.8534,2.3488);
+    addMarker(48.8,2.1333);
+    addMarker(49.2667,-0.7);
+
+    function addMarker(latTest,lngTest){
+      const marker = new google.maps.Marker({
+      // The below line is equivalent to writing:
+      // position: new google.maps.LatLng(-34.397, 150.644)
+      position:{lat:latTest, lng:lngTest},
+      map: map,
+    });
+
+    }
+  }
+</script>
+<script
+      async defer
+      src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBJ-_sW8y-yaYKFfnjzOuo8y6GNkj23iEM&callback=initMap&libraries=&v=weekly">
+</script>
 
 <script lang="ts">
 import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
