2bb0ab63c66954d6d02e77f11ffe298fbca685b5
DylanCampA <dylan.brito@campus.academy>
2021-01-26 14:14:05 +0100

    MAJ Page Artisan

--- src/views/Artisan.vue
+++ src/views/Artisan.vue
@@ -14,7 +14,17 @@
 
     <ion-content :scroll-events="true" ref="content">
       <ion-card>
-        <img src="https://images.unsplash.com/photo-1517433670267-08bbd4be890f?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=972&q=80" alt="" />
+        <ion-slides pager="true" :options="slideOpts">
+          <ion-slide>
+          <img src="https://images.unsplash.com/photo-1517433670267-08bbd4be890f?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=972&q=80" alt="" />
+          </ion-slide>
+          <ion-slide>
+            <img src="" />
+          </ion-slide>
+          <ion-slide>
+            <img src="" />
+          </ion-slide>
+        </ion-slides>
       </ion-card>
 
       <ion-card>
@@ -38,7 +48,7 @@
           Keep close to Nature's heart... and break clear away, once in awhile,
           and climb a mountain or spend a week in the woods. Wash your spirit clean.
         </ion-card-content>
-        <ion-button size="small" style="float: right;">Default</ion-button>
+        <ion-button size="small" style="float: right;">Voir plus</ion-button>
       </ion-card>
 
        <ion-footer class="Footer"></ion-footer>
@@ -54,24 +64,30 @@
 </template>
 
 <script lang="ts">
-import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonCardTitle, IonCardHeader, IonCardContent, IonCard, IonFab, IonFabButton, IonIcon } from '@ionic/vue';
+import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonCardTitle, IonCardHeader, IonCardContent, IonCard, IonFab, IonFabButton, IonIcon, IonSlides, IonSlide } from '@ionic/vue';
 import { chevronUpOutline, closeCircleOutline } from 'ionicons/icons';
 import { ref } from 'vue';
 
 export default  {
   name: 'Artisan',
-  components: { IonHeader, IonToolbar, IonTitle, IonContent, IonPage, IonCardTitle, IonCardHeader, IonCardContent, IonCard, IonFab, IonFabButton, IonIcon },
+  components: { IonHeader, IonToolbar, IonTitle, IonContent, IonPage, IonCardTitle, IonCardHeader, IonCardContent, IonCard, IonFab, IonFabButton, IonIcon, IonSlides, IonSlide },
   setup() {
     const content = ref();
     const scroll = async () => {
       await  content.value.$el.scrollToTop()
       //debugger;
     };
+    const slideOpts = {
+      initialSlide: 0,
+      speed: 300,
+      autoplay:true
+    };
     return{
       chevronUpOutline, 
       closeCircleOutline,
       scroll,
-      content
+      content, 
+      slideOpts
     }
   }
 }
