5a1bae0f8b4918f98d9e0e9720349779587dfc99
QuentinNEP <pro.quentin@hotmail.com>
2021-01-26 09:45:46 +0100

    last clean

--- src/views/Tab1.vue
+++ src/views/Tab1.vue
@@ -18,30 +18,6 @@
   </ion-page>
 </template>
 
-<!-- <script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBJ-_sW8y-yaYKFfnjzOuo8y6GNkj23iEM&callback=initMap"
- type="text/javascript"></script>
-<script lang="ts">
-let map: google.maps.Map;
-
-function initMap(): void {
-  map = new google.maps.Map(document.getElementById("map") as HTMLElement, {
-    center: {lat:48.8534, lng:2.3488},
-    zoom:8,
-  });
-  addMarker(48.8534,2.3488);
-  addMarker(48.8,2.1333);
-  addMarker(49.2667,-0.7);
-
-  function addMarker(latTest,lngTest){
-    const marker = new google.maps.Marker({
-      // The below line is equivalent to writing:
-      // position: new google.maps.LatLng(-34.397, 150.644)
-      position:{lat:latTest, lng:lngTest},
-      map: map,
-    });
-
-  }
-}-->
 <script lang="ts">
 
 
