3c2890b222cd2f09427300800040b1189cc8f433
ManonJK <julienkuentzmanon@gmail.com>
2021-04-18 05:59:46 -0700

    register page

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -2,41 +2,45 @@
   <ion-page>
     <ion-header>
       <ion-toolbar>
-        <ion-title>Authentification</ion-title>
+        <ion-title>Créer un compte</ion-title>
       </ion-toolbar>
     </ion-header>
     <ion-content :fullscreen="true">
-        <form>
-            <ion-grid>
-              <ion-row color="primary" justify-content-center>
-                  <ion-col align-self-center size-md="6" size-lg="5" size-xs="12">
-                    <div text-center>
-                        <h3>Créer un compte</h3>
-                    </div>
-                    <div padding>
-                        <ion-item>
-                          <ion-input  name="name" type="text" placeholder="Nom et Prénom" required></ion-input>
-                        </ion-item>
-                        <ion-item>
-                          <ion-input name="email" type="email" placeholder="Adresse mail" required></ion-input>
-                        </ion-item>
-                        <ion-item>
-                          <ion-input name="password" type="password" placeholder="Mot de passe" required></ion-input>
-                        </ion-item>
-                        <ion-item>
-                          <ion-input name="confirm" type="password" placeholder="Vérification mot de passe" required></ion-input>
-                        </ion-item>
-                    </div>
-                    <div padding>
-                        <ion-button  size="large" type="submit" expand="block">S'enregistrer</ion-button>
-                    </div>
+      <div class="h-100 os-flex flex-column justify-content-around align-items-center">
+        <form class="" style="height: 80%">
+          <ion-grid class="h-100">
+              <ion-row class="h-100" color="primary" justify-content-center>
+                  <ion-col class="h-100 os-flex flex-column justify-content-around" align-self-center size-md="6" size-lg="5" size-xs="12">
+                      <div padding class="os-flex flex-column justify-content-between">
+                          <div class="mb-1 os-flex flex-column justify-content-center">
+                              <ion-label>Nom et prénom</ion-label>
+                              <ion-input name="name" placeholder="Jean Sérien" type="text" required></ion-input>
+                          </div>
+
+                          <div class="mb-1 os-flex flex-column justify-content-center">
+                              <ion-label>Adresse Mail</ion-label>
+                              <ion-input name="email" placeholder="jean.serien@gmail.com" type="email" required></ion-input>
+                          </div>
+
+                          <div class="mb-1 os-flex flex-column justify-content-center">
+                              <ion-label>Mot de passe</ion-label>
+                              <ion-input name="password" placeholder="monsupermotdepasse" type="password" required></ion-input>
+                          </div>
+
+                          <div class="mb-1 os-flex flex-column justify-content-center">
+                              <ion-label>Confirmation du mot de passe</ion-label>
+                              <ion-input name="confirm" placeholder="monsupermotdepasse" type="password" required></ion-input>
+                          </div>
+                      </div>
+                      <div padding>
+                          <ion-button  size="large" type="submit" expand="block">S'enregistrer</ion-button>
+                          <a href="/auth">J'ai déjà un compte, je me connecte</a>
+                      </div>
                   </ion-col>
               </ion-row>
-            </ion-grid>
+          </ion-grid>
         </form>
-        <ion-item>
-            <a href="/auth">J'ai déjà un compte, je me connecte</a>
-        </ion-item>
+      </div>
     </ion-content>
   </ion-page>
 </template>
@@ -52,10 +56,10 @@ ion-button{
 }
 </style>
 <script lang="ts">
-import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonButton, IonInput, IonGrid, IonItem, IonRow } from '@ionic/vue';
+import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonButton, IonInput, IonGrid, IonRow } from '@ionic/vue';
 
 export default  {
   name: 'Register',
-  components: { IonHeader, IonToolbar, IonTitle, IonContent, IonPage, IonButton, IonInput, IonGrid, IonItem, IonRow }
+  components: { IonHeader, IonToolbar, IonTitle, IonContent, IonPage, IonButton, IonInput, IonGrid, IonRow }
 }
 </script>       
