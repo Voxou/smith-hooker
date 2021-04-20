6822ffe4675470b25146f716dc6e1ed125f7dfd7
DylanCampA <dylan.brito@campus.academy>
2021-04-20 12:45:18 +0200

    Amélioration responsive

--- src/views/Auth.vue
+++ src/views/Auth.vue
@@ -9,33 +9,40 @@
         <ion-content :fullscreen="true">
             <div class="h-100 os-flex flex-column justify-content-around align-items-center">
                 <form class="" style="height: 80%">
-                    <div padding  class="os-flex flex-column justify-content-between">
-                        <div class="mb-1 os-flex flex-column justify-content-center">
-                                <ion-label>Login</ion-label>
-                                <ion-input name="email" placeholder="Adresse mail" type="email" required></ion-input>
-                        </div>
+                    <ion-grid class="h-100">
+                        <ion-row class="h-100" color="primary" justify-content-center>
+                            <ion-col class="h-100 os-flex flex-column justify-content-around" align-self-center>
+                                <div padding  class="os-flex flex-column justify-content-between">
+                                    <div class="mb-1 os-flex flex-column justify-content-center">
+                                        <ion-label>Login</ion-label>
+                                        <ion-input name="email" placeholder="Adresse mail" type="email" required></ion-input>
+                                    </div>
 
-                        <div class="mb-1 os-flex flex-column justify-content-center">
-                                <ion-label>Mot de passe</ion-label>
-                                <ion-input name="password" placeholder="Mot de passe" type="password" required></ion-input>
-                        </div>
-                    </div>
-                    <div padding>
-                        <ion-button  size="large" type="submit" expand="block">Connexion</ion-button>
-                        <a href="/register">Je n'ai pas de compte, j'en créé un</a>
-                    </div>
+                                    <div class="mb-1 os-flex flex-column justify-content-center">
+                                        <ion-label>Mot de passe</ion-label>
+                                        <ion-input name="password" placeholder="Mot de passe" type="password" required></ion-input>
+                                    </div>
+                                </div>
+                                <div padding>
+                                    <ion-button  size="large" type="submit" expand="block">Connexion</ion-button>
+                                    <a href="/register">Je n'ai pas de compte, j'en créé un</a>
+                                </div>
+                            </ion-col>
+                        </ion-row>
+                    </ion-grid>
                 </form>
             </div>
         </ion-content>
+        
     </ion-page>
 </template>
 
 <script>
-import { IonPage, IonHeader, IonToolbar, IonTitle, IonImg, IonContent, IonLabel, IonInput, IonButton } from '@ionic/vue';
+import { IonPage, IonHeader, IonToolbar, IonTitle, IonImg, IonContent, IonGrid, IonRow, IonCol, IonLabel, IonInput, IonButton } from '@ionic/vue';
 import { defineComponent, computed } from 'vue';
 
 export default defineComponent({
-    components: { IonPage, IonHeader, IonToolbar, IonTitle, IonImg, IonContent, IonLabel, IonInput, IonButton },
+    components: { IonPage, IonHeader, IonToolbar, IonTitle, IonImg, IonContent, IonGrid, IonRow, IonCol, IonLabel, IonInput, IonButton },
     setup() {
     const logo = computed(()=> require('../../public/assets/images/logo_en_noir.svg'));
     return{
